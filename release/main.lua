101bee3b343830cc17ab274fda604836be4692a5
BIGSING <pushebp@163.com>
2026-07-24 13:38:49 +0800

    1

---
+++ release/main.lua
@@ -0,0 +1,12 @@
+local star = require "star"
+
+local root, file = star.path()
+local starVersion, luaVersion = star.version()
+
+print(("Star %s / %s"):format(starVersion, luaVersion))
+print("当前脚本：" .. root .. file)
+
+for index, value in ipairs({...}) do
+    print(("参数[%d]：%s"):format(index, value))
+end
+star.pause()
